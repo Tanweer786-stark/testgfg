a093a1a41c2157a8b4d9f49936cba9c6be6815c1
TANWEER IQBAL <118075528+Tanweer786-stark@users.noreply.github.com>
2025-08-07 22:41:25 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Palindrome Linked List/palindrome-linked-list.cpp
@@ -0,0 +1,37 @@
+/*
+struct Node {
+  int data;
+  struct Node *next;
+  Node(int x) {
+    data = x;
+    next = NULL;
+  }
+};
+*/
+
+class Solution {
+  private:
+    bool checkpalindrome(vector<int> arr){
+        int n = arr.size();
+        int s = 0;
+        int e = n-1;
+        while(s<=e){
+            if(arr[s] != arr[e]){
+                return 0;
+            }
+            s++;
+            e--;
+        }
+        return 1;
+    }
+  public:
+    bool isPalindrome(Node *head) {
+        vector<int> arr;
+        Node* temp = head;
+        while(temp != NULL){
+            arr.push_back(temp -> data);
+            temp = temp -> next;
+        }
+        return checkpalindrome(arr);
+    }
+};
